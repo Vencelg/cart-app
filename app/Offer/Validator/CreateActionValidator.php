96a49b7094f93a46ece69d8cb104b0a40c9a7c2e
vencelg <gazdavaclav@gmail.com>
2022-06-22 10:36:05 +0200

    18 [feat] add Create and Update validators to the offer module

--- app/Offer/Validator/CreateActionValidator.php
+++ app/Offer/Validator/CreateActionValidator.php
@@ -10,7 +10,11 @@ class CreateActionValidator extends AbstractValidator
     public function rules(): array
     {
         return [
-
+            'start' => 'required|string',
+            'finish' => 'required|string',
+            'price' => 'required|integer',
+            'space' => 'required|integer',
+            'departure' => 'required|string'
         ];
     }
 }
