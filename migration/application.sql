4922ae1338c1c31d5da5c43bae9fce0af07abfc9
vencelg <gazdavaclav@gmail.com>
2022-06-22 12:42:56 +0200

    20 [feat] add age and profile_picture columns to user table

--- migration/application.sql
+++ migration/application.sql
@@ -2,19 +2,21 @@ CREATE SCHEMA IF NOT EXISTS cart_app;
 
 CREATE TABLE IF NOT EXISTS cart_app."user"
 (
-    id            SERIAL       NOT NULL,
-    name          VARCHAR(100) NOT NULL,
-    surname       VARCHAR(150) NOT NULL,
-    email         VARCHAR(255)
+    id              SERIAL       NOT NULL,
+    name            VARCHAR(100) NOT NULL,
+    surname         VARCHAR(150) NOT NULL,
+    email           VARCHAR(255)
         CONSTRAINT user_email_key
-            UNIQUE             NOT NULL,
-    password      VARCHAR(255) NOT NULL,
-    api_key       VARCHAR(32)  NOT NULL,
-    gender        varchar(255) NOT NULL,
-    created       TIMESTAMP WITH TIME ZONE,
-    created_by_id INT,
-    updated       TIMESTAMP WITH TIME ZONE,
-    updated_by_id INT,
+            UNIQUE               NOT NULL,
+    password        VARCHAR(255) NOT NULL,
+    api_key         VARCHAR(32)  NOT NULL,
+    gender          varchar(255) NOT NULL,
+    age             INT          NOT NULL,
+    profile_picture varchar(255) NOT NULL,
+    created         TIMESTAMP WITH TIME ZONE,
+    created_by_id   INT,
+    updated         TIMESTAMP WITH TIME ZONE,
+    updated_by_id   INT,
     PRIMARY KEY (id),
     FOREIGN KEY (created_by_id) REFERENCES cart_app."user" (id) ON UPDATE CASCADE ON DELETE SET NULL,
     FOREIGN KEY (updated_by_id) REFERENCES cart_app."user" (id) ON UPDATE CASCADE ON DELETE SET NULL
