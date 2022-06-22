46f2463a7e814efd0fb8627542b5c4dd90680210
vencelg <gazdavaclav@gmail.com>
2022-06-22 09:36:57 +0200

    14 [feat] add CreateActionValidator.php and UpdateActionValidator.php for action validation

---
+++ app/User/Validator/CreateActionValidator.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace CartApp\User\Validator;
+
+use CartApp\Core\Validator\AbstractValidator;
+
+class CreateActionValidator extends AbstractValidator
+{
+
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string',
+            'surname' => 'required|string',
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+            'gender' => 'required|string',
+        ];
+    }
+}
